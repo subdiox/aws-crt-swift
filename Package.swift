070f44f965906f809a3edabb1892da22c4942382
subdiox <subdiox@gmail.com>
2024-05-31 02:14:31 +0900

    Force cxx support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.10
 import PackageDescription
 
 let excludesFromAll = ["tests", "cmake", "CONTRIBUTING.md",
@@ -269,7 +269,8 @@ packageTargets.append(contentsOf: [
         path: "Source/AwsCommonRuntimeKit",
         resources: [
             .copy("PrivacyInfo.xcprivacy")
-        ]
+        ],
+        swiftSettings: [.interoperabilityMode(.Cxx)]
     ),
     .testTarget(
         name: "AwsCommonRuntimeKitTests",
@@ -282,7 +283,8 @@ packageTargets.append(contentsOf: [
     .executableTarget(
         name: "Elasticurl",
         dependencies: ["AwsCommonRuntimeKit"],
-        path: "Source/Elasticurl"
+        path: "Source/Elasticurl",
+        swiftSettings: [.interoperabilityMode(.Cxx)]
     )
 ] )
 package.targets = packageTargets
